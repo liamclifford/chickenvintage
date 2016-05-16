a0ac3e90d0c34a943bc94efd1be3a9a3bf323149
Liam Clifford <lsclifford@outlook.com>
2016-05-16 20:50:36 +0100

    First commit

--- index/views.py
+++ index/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+	return render(request, 'index/index.html')
+
+
+def shop(request):
+	return render(request, 'index/shop.html')
